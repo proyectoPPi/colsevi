1fe8c2d2e09225975c648ec5d3262bbe48f16190
Brayan <Brayan@PC-Brayan>
2016-03-22 14:50:56 -0500

    Desarrollo Ingredientes
    Se construye desarrollo de ingredientes con cambios en herramientas.js
    para validacion de subdatos

--- src/com/colsevi/application/FiltroAutenticacion.java
+++ src/com/colsevi/application/FiltroAutenticacion.java
@@ -73,6 +73,7 @@ public class FiltroAutenticacion implements Filter, Serializable{
 			path.startsWith("/General/Establecimiento") || 
 			path.startsWith("/Pedido/Visualizar") ||
 			path.startsWith("/Ingrediente/Clasificar") ||
+			path.startsWith("/Ingrediente/Ing") ||
 			path.startsWith(LOGIN)
 			
 			){
