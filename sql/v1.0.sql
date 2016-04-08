1affea1953c0e923a38cdc0b845eef6919244821
Brayan <Brayan@10.100.74.95>
2016-04-08 13:49:35 -0500

    SQL

--- sql/v1.0.sql
+++ sql/v1.0.sql
@@ -14,6 +14,8 @@ CREATE TABLE persona(
     tipo_doc INT NOT NULL,
     documento VARCHAR (30) NOT NULL,
     genero VARCHAR (1) NOT NULL,
+    nombre VARCHAR(60),
+    apellido VARCHAR(60),
     PRIMARY KEY(id_persona),
 	UNIQUE (documento),
     CONSTRAINT fk_tipoDocPersona FOREIGN KEY (tipo_doc)
@@ -36,21 +38,21 @@ CREATE TABLE usuario(
 
 CREATE TABLE pagina(
 	id_pagina INT AUTO_INCREMENT,
-    nivel INTEGER NOT NULL,
     codigo VARCHAR (30) NOT NULL,
     url VARCHAR (50) NOT NULL,
     nombre VARCHAR (50) NOT NULL,
     icono VARCHAR (50),
-    PRIMARY KEY(id_pagina)
+    padrePagina INT,
+    PRIMARY KEY(id_pagina),
+    CONSTRAINT fk_PaginaPadre FOREIGN KEY (padrePagina)
+	REFERENCES pagina(id_pagina)
 );
 
 CREATE TABLE rol(
 	id_rol INT AUTO_INCREMENT,
     nombre_rol VARCHAR(40) NOT NULL,
     codigo_rol VARCHAR(80) NOT NULL,
-    pagina_defecto INT NOT NULL,
 	PRIMARY KEY(id_rol),
-    CONSTRAINT fk_paginadefecto FOREIGN KEY (pagina_defecto)
 	REFERENCES pagina(id_pagina)
 );
 
@@ -78,22 +80,13 @@ CREATE TABLE establecimiento(
 	id_establecimiento INT AUTO_INCREMENT,
     nombre VARCHAR (30) NOT NULL,
     descripcion VARCHAR (30) NOT NULL,
+    hora_inicio VARCHAR(7),
+    hora_fin VARCHAR(7),
+    id_direccion INT,
+    id_telefono INT,
+    id_correo INT,
     PRIMARY KEY(id_establecimiento)
 );
-
-INSERT INTO tipo_documento VALUES(null,'Cedula','Cedula de Ciudadania');
-INSERT INTO persona VALUES(null,1,960102613128,'F');
-INSERT INTO usuario VALUES(null,'ppiAdmin',1,'711383a59fda05336fd2ccf70c8059d1523eb41a','T','brayycard@hotmail.com','F');
-INSERT INTO pagina VALUES(null,0,'ESTABLECIMIENTO','/General/Establecimiento.html','Establecimiento');
-INSERT INTO rol VALUES(null,1,'ADMIN',1);
-INSERT INTO pagina_x_rol VALUES(1,1);
-INSERT INTO usuario_x_rol VALUES(1,1);
-
-ALTER TABLE establecimiento ADD COLUMN hora_inicio VARCHAR(7);
-ALTER TABLE establecimiento ADD COLUMN hora_fin VARCHAR(7);
-ALTER TABLE establecimiento ADD COLUMN id_direccion INT;
-ALTER TABLE establecimiento ADD COLUMN id_telefono INT;
-ALTER TABLE establecimiento ADD COLUMN id_correo INT; 
 
 CREATE TABLE tipo_telefono(
 	id_tipo_telefono INT AUTO_INCREMENT,
@@ -163,10 +156,13 @@ CREATE TABLE proveedor (
 CREATE TABLE compra(
 	id_compra INT AUTO_INCREMENT,
     id_proveedor INT NOT NULL,
+    id_establecimiento INT,
     valor DECIMAL(12,2) NOT NULL,
     fecha_compra DATETIME NOT NULL,
     pagado BOOLEAN DEFAULT FALSE,
+    motivo VARCHAR(200),
     PRIMARY KEY(id_compra),
+    CONSTRAINT fk_compra_establecimiento FOREIGN KEY (id_establecimiento) REFERENCES establecimiento(id_establecimiento)
 	CONSTRAINT fk_compra_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor)
 );
 
@@ -206,7 +202,7 @@ CREATE TABLE compra_x_ingrediente(
     CONSTRAINT fk_comprasXIngrediente3 FOREIGN KEY (id_tipo_peso) REFERENCES tipo_peso(id_tipo_peso)
 );
 
-ALTER TABLE compra_x_ingrediente AUTO_INCREMENT=1000;
+ALTER TABLE compra_x_ingrediente AUTO_INCREMENT = 1000;
 
 CREATE TABLE tipo_producto(
     id_tipo_producto INT AUTO_INCREMENT,
@@ -257,6 +253,7 @@ CREATE TABLE catalogo(
 	id_establecimiento INT,
     nombre VARCHAR(50),
     descripcion VARCHAR(80),
+    vigente BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(id_catalogo),
     CONSTRAINT fk_establecimientoCatalogo FOREIGN KEY (id_establecimiento) REFERENCES establecimiento(id_establecimiento)
 );
@@ -298,93 +295,3 @@ CREATE TABLE detalle_pedido(
     CONSTRAINT fk_secuenciaPedido FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
     CONSTRAINT fk_detalleProductoPed FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 );
-
-CREATE TABLE inventario(
-    id_inventario INT AUTO_INCREMENT,
-	id_establecimiento INT,
-    id_producto INT,
-    disponible INT,
-    comprometido INT,
-    PRIMARY KEY(id_inventario),
-    CONSTRAINT fk_EstablecimientoInventario FOREIGN KEY (id_establecimiento) REFERENCES establecimiento(id_establecimiento),
-    CONSTRAINT fk_productoInventario FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-);
-
-CREATE TABLE categoria_modulo(
-    id_categoria_modulo INT AUTO_INCREMENT,
-	nombre VARCHAR(30) NOT NULL,
-    tabla VARCHAR(60) DEFAULT NULL,
-    PRIMARY KEY(id_categoria_modulo)
-);
-
-CREATE TABLE pago(
-    id_pago INT AUTO_INCREMENT,
-	id_categoria_modulo INT,
-    documento INT,
-    pendiente DECIMAL(12,2),
-    fecha DATETIME,
-    PRIMARY KEY(id_pago),
-    CONSTRAINT fk_categoriaPago FOREIGN KEY (id_categoria_modulo) REFERENCES categoria_modulo(id_categoria_modulo)
-);
-
-CREATE TABLE detalle_pago(
-    id_detalle_pago INT AUTO_INCREMENT,
-	id_pago INT,
-    fecha DATETIME,
-    valor_pagado DECIMAL(12,2),
-    observacion VARCHAR(150),
-    PRIMARY KEY(id_detalle_pago),
-    CONSTRAINT fk_detallePago FOREIGN KEY (id_pago) REFERENCES pago(id_pago)
-);
-
-CREATE TABLE deuda(
-    id_deuda INT AUTO_INCREMENT,
-	id_categoria_modulo INT,
-    documento INT,
-    fecha_registro DATETIME,
-    valor_deuda DECIMAL(12,2),
-    observacion VARCHAR(250),
-    PRIMARY KEY(id_deuda),
-    CONSTRAINT fk_categoriaDeuda FOREIGN KEY (id_categoria_modulo) REFERENCES categoria_modulo(id_categoria_modulo)
-);
-
-CREATE TABLE cobro_categoria(
-	id_cobro_categoria INT AUTO_INCREMENT,
-    nombre VARCHAR(50),
-    PRIMARY KEY(id_cobro_categoria)
-);
-
-CREATE TABLE cobro(
-    id_cobro INT AUTO_INCREMENT,
-	id_cobro_categoria INT,
-    id_deuda INT,
-    fecha_cobro DATETIME,
-    comentario VARCHAR(250),
-    PRIMARY KEY(id_cobro),
-    CONSTRAINT fk_cobroDeuda FOREIGN KEY (id_deuda) REFERENCES deuda(id_deuda),
-    CONSTRAINT fk_cobrocategoria FOREIGN KEY (id_cobro_categoria) REFERENCES cobro_categoria(id_cobro_categoria)
-);
-
-CREATE TABLE motivo(
-	id_motivo INT AUTO_INCREMENT,
-    nombre VARCHAR(70),
-    PRIMARY KEY(id_motivo)
-);
-
-CREATE TABLE movimiento_inventario(
-    id_movimiento_inventario INT AUTO_INCREMENT,
-	transicion INT,
-    documento INT,
-    id_motivo INT,
-    id_categoria_modulo INT,
-    cantidad INT,
-    PRIMARY KEY(id_movimiento_inventario),
-    CONSTRAINT fk_movimientoCategoria FOREIGN KEY (id_categoria_modulo) REFERENCES categoria_modulo(id_categoria_modulo),
-    CONSTRAINT fk_motivoMovimiento FOREIGN KEY (id_motivo) REFERENCES motivo(id_motivo)
-);
-
-ALTER TABLE catalogo ADD COLUMN vigente BOOLEAN DEFAULT false;
-ALTER TABLE persona ADD COLUMN nombre VARCHAR(60);
-ALTER TABLE persona ADD COLUMN apellido VARCHAR(60);
-
-ALTER TABLE compra ADD COLUMN motivo VARCHAR(200);
